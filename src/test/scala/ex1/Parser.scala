e1c72bf5de816ffefc1c20af71a42a2f690bd34c
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-04-14 13:11:15 +0200

    update tests

--- src/test/scala/ex1/Parser.scala
+++ src/test/scala/ex1/Parser.scala
@@ -3,6 +3,7 @@ package ex1
 import ex1.*
 import org.junit.Assert.*
 import org.junit.Test
+import ex1.Parsers.charParser
 
 class ParserTests:
   def parser = new BasicParser(Set('a', 'b', 'c'))
@@ -12,7 +13,7 @@ class ParserTests:
   def parserNTC = new NotTwoConsecutiveParser(Set('X', 'Y', 'Z'))
   // note we do not need a class name here, we use the structural type
   def parserNTCNE = new BasicParser(Set('X', 'Y', 'Z')) with NotTwoConsecutive[Char] with NonEmpty[Char]
-  def sparser: Parser[Char] = ??? // "abc".charParser()
+  def sparser: Parser[Char] = "abc".charParser()
 
   @Test
   def testBasicParser =
